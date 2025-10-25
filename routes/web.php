624b8295f705351ad53ffb754da83af2c8e09cbf
Eng Amr Elgazar <158527211+engamrelgazar@users.noreply.github.com>
2025-10-25 20:15:38 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use Botble\Base\Facades\AdminHelper;
-use Botble\Base\Http\Middleware\DisableInDemoModeMiddleware;
-use Botble\DataSynchronize\Http\Controllers\DataSynchronizeController;
-use Botble\DataSynchronize\Http\Controllers\UploadController;
+use Tiryaq\Base\Facades\AdminHelper;
+use Tiryaq\Base\Http\Middleware\DisableInDemoModeMiddleware;
+use Tiryaq\DataSynchronize\Http\Controllers\DataSynchronizeController;
+use Tiryaq\DataSynchronize\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 AdminHelper::registerRoutes(function () {
